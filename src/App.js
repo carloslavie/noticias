f697cfec230d8de1b8b86910c91af32071b9e204
carloslavie <carloslavie@gmail.com>
2021-03-10 17:55:12 -0300

    cambio de api a gnews

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ function App() {
   useEffect(() => {
 
     const consultarAPI = async ()=>{
-        const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=84729dc602544e0295d7ce0696789f32`
+      const url2 = `https://gnews.io/api/v4/top-headlines?topic=${categoria}&country=us&token=97d5527c6dc10d7194e0202c2b071f83`;
+        // const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=84729dc602544e0295d7ce0696789f32`;
 
-        const respuesta = await fetch(url)
+        const respuesta = await fetch(url2)
         const noticias = await respuesta.json();
-
+        console.log(noticias)
         guardarNoticias(noticias.articles);
 
     }
@@ -43,3 +44,5 @@ function App() {
 }
 
 export default App;
+
+
